324768beeba8d9154d73c27918708f4c2c6320ca
Clayton <cmpetersen@hotmail.co.za>
2019-04-27 14:43:57 +0200

    Working on classes

--- src/main/java/c/domain/employee/Qualification.java
+++ src/main/java/c/domain/employee/Qualification.java
@@ -1,4 +1,63 @@
 package c.domain.employee;
 
+import javafx.util.Builder;
+
 public class Qualification {
+
+
+    private int qualificationID;
+    private String qualificationName;
+    private String instituteName;
+    private String qualificationDuration;
+
+    public Qualification(){
+
+    }
+
+    public Qualification(Builder builder){
+
+        this.qualificationID = builder.qualificationID;
+        this.qualificationName = builder.qualificationName;
+        this.instituteName = builder.instituteName;
+        this.qualificationDuration = builder.qualificationDuration;
+
+    }
+
+    public int getQualificationID() {
+        return qualificationID;
+    }
+
+    public String getQualificationName() {
+        return qualificationName;
+    }
+
+    public String getInstituteName() {
+        return instituteName;
+    }
+
+    public String getQualificationDuration() {
+        return qualificationDuration;
+    }
+
+    public static class Builder{
+
+        private int qualificationID;
+        private String qualificationName;
+        private String instituteName;
+        private String qualificationDuration;
+
+
+        public Builder qualificationID(int qualificationID){
+
+            this.qualificationID = qualificationID;
+            return this;
+
+        }
+
+    }
+
+
+
+
+
 }
