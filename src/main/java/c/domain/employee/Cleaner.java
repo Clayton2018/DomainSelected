0bd159bdccd1d0527eec7869717cfe93f7b1d634
Clayton <cmpetersen@hotmail.co.za>
2019-05-19 10:49:02 +0200

    Adding controllers and services

--- src/main/java/c/domain/employee/Cleaner.java
+++ src/main/java/c/domain/employee/Cleaner.java
@@ -1,6 +1,75 @@
 package c.domain.employee;
 
-public class Cleaner {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Cleaner extends Employee{
+
+      private int[] officeRoom;
+
+
+    public Cleaner(){
+
+    }
+
+    public Cleaner(CleanerBuilder builder){
+
+        super(builder);
+        this.officeRoom = builder.officeRoom;
+
+    }
+
+    public int[] getOfficeRoom() {
+        return officeRoom;
+    }
+
+    public void setOfficeRoom(int[] officeRoom) {
+        this.officeRoom = officeRoom;
+    }
+
+    public static class CleanerBuilder extends Employee.Builder{
+
+        private int[] officeRoom;
+
+        public CleanerBuilder OfficeRoom(int[] officeRoom){
+
+            this.officeRoom = officeRoom;
+            return this;
+
+        }
+
+        @Override
+        public String toString() {
+
+            return "CleanerBuilder{" + " Office:"
+                    + Arrays.toString(officeRoom) + "}\n" +
+                    super.toString();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if(this == obj) return true;
+            if(obj == null || getClass() != obj.getClass()) return false;
+
+            CleanerBuilder that = (CleanerBuilder) obj;
+
+            return Arrays.equals(officeRoom, that.officeRoom);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(officeRoom);
+        }
+
+        @Override
+        public Employee build(){
+
+            return new Cleaner(this);
+
+        }
+
+    }
 
 
 
--- src/main/java/c/domain/employee/Mechanic.java
+++ src/main/java/c/domain/employee/Mechanic.java
@@ -6,7 +6,40 @@ import java.util.List;
 
 public class Mechanic extends Employee {
 
+
     private String duties;
     private List<Vehicle> vehicles;
+
+
+    public Mechanic(){
+
+    }
+
+    public Mechanic(MechanicBuilder builder){
+
+        super(builder);
+        this.duties = builder.duties;
+        this.vehicles = builder.vehicles;
+
+
+    }
+
+
+    public String getDuties() {
+        return duties;
+    }
+
+    public void setDuties(String duties) {
+        this.duties = duties;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
 
 }
