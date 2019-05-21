fe33e294e56333d032bbc4516b6f019e6077b0af
Clayton <cmpetersen@hotmail.co.za>
2019-05-21 20:46:04 +0200

    Adding service implementations

--- src/main/java/c/service/employee/impl/VehicleInspectorServiceImpl.java
+++ src/main/java/c/service/employee/impl/VehicleInspectorServiceImpl.java
@@ -1,8 +1,12 @@
 package c.service.employee.impl;
 
+import c.domain.employee.VehicleInspector;
 import c.repositories.employees.VehicleInspectorRepository;
 import c.service.employee.VehicleInspectorService;
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service("VehicleInspectorServiceImpl")
 public class VehicleInspectorServiceImpl implements VehicleInspectorService {
@@ -18,8 +22,39 @@ public class VehicleInspectorServiceImpl implements VehicleInspectorService {
 
     public VehicleInspectorServiceImpl getService() {
 
-        if(service == null){}
+        if(service == null){
+
+            service = new VehicleInspectorServiceImpl();
+
+        }
 
         return service;
+    }
+
+    @Override
+    public Set<VehicleInspector> getAll() {
+        return repository.getAll();
+    }
+
+    @Override
+    public VehicleInspector create(VehicleInspector vehicleInspector) {
+        return repository.create(vehicleInspector);
+    }
+
+    @Override
+    public VehicleInspector read(Integer integer) {
+        return repository.read(integer);
+    }
+
+    @Override
+    public VehicleInspector update(VehicleInspector vehicleInspector) {
+        return repository.update(vehicleInspector);
+    }
+
+    @Override
+    public void delete(Integer integer) {
+
+        repository.delete(integer);
+
     }
 }
