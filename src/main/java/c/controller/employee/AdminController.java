f323893033a695d53d58da97a37af580693c7749
Clayton <cmpetersen@hotmail.co.za>
2019-10-04 23:23:13 +0200

    completing incomplete code

--- src/main/java/c/controller/employee/AdminController.java
+++ src/main/java/c/controller/employee/AdminController.java
@@ -1,4 +1,54 @@
 package c.controller.employee;
 
+import c.Factory.employee.AdministratorFactory;
+import c.domain.employee.Administrator;
+import c.service.employee.impl.AdministratorServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("administrator")
 public class AdminController {
+
+    @Autowired
+    private AdministratorServiceImpl administratorService;
+
+    @PostMapping("/new")
+    public Administrator create(@RequestBody Administrator administrator){
+
+        return administratorService.create(administrator);
+
+    }
+
+    @GetMapping(path = "/find/{id}")
+    public Administrator findById(@PathVariable int id){
+
+        Administrator a = administratorService.read(id);
+        return a;
+
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody Administrator administrator){
+
+        administratorService.update(administrator);
+
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public void delete(@PathVariable int id){
+
+        administratorService.delete(id);
+
+    }
+
+    @GetMapping("/getAll")
+    public Set<Administrator> getAll(){
+
+        return administratorService.getAll();
+
+    }
+
 }
